7715ba4e8d220be0fe7e79756ae7f112ef405747
YoungAndDo <yd199323@gmail.com>
2019-08-14 20:19:16 +0900

    독립원고 프로젝트 초안 저장

---
+++ index.js
@@ -0,0 +1,64 @@
+const startString = '대한독립만세';
+const upper = 'upper';
+
+const addSpaces = spaceLength => {
+    let spaces = '';
+    for (let i = 0; i < spaceLength; i++) {
+        spaces += ' ';
+    }
+    return spaces;
+}
+
+const makeManuscript = string => {
+    let nlIndex = string.indexOf('\n');
+    while (nlIndex != -1) {
+        const nlInOneLine = nlIndex % 15;
+        string = string.slice(0, nlIndex) + addSpaces(15 - (nlInOneLine)) + string.slice(nlIndex+1);
+        nlIndex = string.indexOf('\n');
+    }
+    if (string.length < 150) {
+        string += addSpaces(150 - string.length);
+    } else {
+        string.slice(0, 150);
+    }
+    let domTree = '', count = 135;
+    for (let i = 0; i < 10; i++) {
+        domTree = domTree + `<span class="${upper}">${string[count] == ' ' ? '&nbsp;' : string[count]}</span>`;
+        count = count - 15;
+    }
+    domTree += '<br>';
+    count += 151;
+    while (count < 150) {
+        for (let i = 0; i < 10; i++) {
+            domTree = domTree + `<span>${string[count] == ' ' ? '&nbsp;' : string[count]}</span>`;
+            count = count - 15;
+        }
+        count += 151;
+        domTree += '<br>';
+    }
+    document.querySelector('#paper').innerHTML = domTree;
+};
+
+document.querySelector('#input').addEventListener('keyup', () => {
+    makeManuscript(document.querySelector('#input').value);
+});
+
+document.querySelector('#font-selector').addEventListener('change', () => {
+    const selectedFont = document.querySelector('#font-selector').value;
+    document.querySelector('#paper').style.fontFamily = selectedFont;
+    document.querySelectorAll('.title').forEach(item => item.style.fontFamily = selectedFont);
+});
+
+document.querySelector('#saveButton').addEventListener('click', () => {
+    html2canvas(document.querySelector('#paperContainer')).then(canvas => {
+        const link = document.createElement('a');
+        const filename = new Date().toLocaleString();
+        document.body.appendChild(link);
+        link.download = `${filename}.png`;
+        link.href = canvas.toDataURL('image/png');
+        link.target = '_blank';
+        link.click();
+    });    
+});
+
+makeManuscript(startString);
