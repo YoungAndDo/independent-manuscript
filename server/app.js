af873d470a5a978fce8cebe6b024890697fe32ff
YoungAndDo <yd199323@gmail.com>
2019-09-12 16:52:46 +0900

    이미지 저장 방식을 한 파일 덮어쓰기로 변경
    기존 이미지 업로드, 다운로드 방식이 유저의 쿠키 값에 따라 저장된
    이미지를 보내는 것이었으나, 페이스북 인앱 브라우저에선 이미지 다운로드
    할 때 크롬과 같은 다른 브라우저로 이동하게 되므로, 임시조치 방편으로
    정해진 경로의 파일을 업로드하고 다운받는 방식으로 변경함

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const multer = require('multer');
 const cookieParser = require('cookie-parser');
-const uuidv1 = require('uuid/v1');
 const fs = require('fs');
 const path = require('path');
 
@@ -9,7 +8,7 @@ const app = express();
 const handler = multer();
 
 const PORT = process.env.PORT || 3000;
-const shortid = () => uuidv1().slice(0, 8);
+const imgPath = path.join(__dirname, '../public/img/saveImage.png');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -17,10 +16,8 @@ app.use(cookieParser());
 app.use(express.static('public'));
 
 app.post('/upload', handler.single('file'), (req, res, next) => {
-  const sid = (req.headers.cookie ? req.headers.cookie.split('=')[1] : shortid());
-  const picStream = fs.createWriteStream(path.join(__dirname, `../public/img/${sid}.png`));
+  const picStream = fs.createWriteStream(imgPath);
   picStream.on('close', () => {
-    res.cookie('_sid', sid);
     res.send({message: 'finished'});
   });
   picStream.write(req.file.buffer);
@@ -28,10 +25,8 @@ app.post('/upload', handler.single('file'), (req, res, next) => {
 });
 
 app.get('/download', (req, res, next) => {
-  if (req.headers.cookie) {
-    const sid = req.headers.cookie.split('=')[1];
-    res.download(path.join(__dirname, `../public/img/${sid}.png`), `${sid}.png`);
-  }
+  const filename = `${new Date().toLocaleString()}.png`;
+  res.download(imgPath, filename);
 });
 
 // error handling
